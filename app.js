5ceb08382c8c8c6c868cebf63631dc6ff4d2b6f8
Shubham-503 <kr.shubham025@gmail.com>
2024-10-22 19:37:38 +0530

    initial setup

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser());
+
+// routes
+app.get("/", (req, res) => {
+  res, json({ message: "home routes" });
+});
+
+export default app;
